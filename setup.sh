350844b939623941e0e8ed5fef68537646945cf5
Snipher Marube <sniphermarube@gmail.com>
2025-07-26 15:30:48 +0300

    project deployment setup

--- setup.sh
+++ setup.sh
@@ -7,6 +7,5 @@ pip install setuptools
 pip install -r requirements.txt
 
 # Run Django management commands
-python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
