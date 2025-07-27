0915b871dcfe96813d409cd62486a0cb6432f09c
Snipher Marube <sniphermarube@gmail.com>
2025-07-27 14:40:21 +0300

    settings

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.urls import re_path
+from django.views.static import serve
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
@@ -48,3 +51,10 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^swagger(?P<path>.*)$', serve, {
+            'document_root': os.path.join(settings.STATIC_ROOT, 'drf-yasg'),
+        }),
+    ]
