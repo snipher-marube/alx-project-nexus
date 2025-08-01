bcfdf965cadf60f2976c645a519c94d4f13d7189
Snipher Marube <sniphermarube@gmail.com>
2025-08-01 10:26:27 +0300

    resolve the conflict

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -84,10 +84,6 @@ CSRF_COOKIE_SAMESITE = 'Lax'
 SESSION_COOKIE_SAMESITE = 'Lax'
 
 # cloudinary settings
-<<<<<<< HEAD
-
-=======
->>>>>>> 95eed58fc0610d680eac4a5fdd9991de1aa843e9
 cloudinary.config(
     cloud_name=config('CLOUDINARY_CLOUD_NAME'),
     api_key=config('CLOUDINARY_API_KEY'),
